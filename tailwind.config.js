23ca69a7022de0c62dd72c54437b0d63f5938d75
manafGeokatalis <abdul.manaaf.ma@gmail.com>
2024-07-31 18:34:55 +0700

    selesai sampai langkah publish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/pages/*.html",    
+    "./src/**/*.js",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: '#1E3A8A',
+        briBlue: '#DBEAFE',        
+        hoverBlue: '#A3CAFF',
+        briWarning: '#F4BF37',
+        bgStart: '#BEDCFE',
+        blacky: '#272727'
+      },
+      fontFamily: {
+        sans: ['DM Sans', 'sans-serif'],
+        opensans: ['Quicksand', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
-
